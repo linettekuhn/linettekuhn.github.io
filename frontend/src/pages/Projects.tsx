3a26c603c15d401c16b3110c04d2b0f24cd47369
Linette Kühn <139412656+linettekuhn@users.noreply.github.com>
2025-08-17 16:08:56 -0400

    Update package dependencies, and enhance CSS styles for consistency

--- frontend/src/pages/Projects.tsx
+++ frontend/src/pages/Projects.tsx
@@ -1,11 +1,15 @@
 import Navbar from "../components/Navbar";
+import { IoConstruct } from "react-icons/io5";
 
 export default function Projects() {
   return (
     <>
       <Navbar />
       <div className="content">
-        <h1>Under construction!</h1>
+        <h1>
+          <IoConstruct />
+          Under construction!
+        </h1>
       </div>
     </>
   );

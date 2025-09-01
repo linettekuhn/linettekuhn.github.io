1174ac873787cc777ae67d4815d23d465665cfa5
Linette Kühn <139412656+linettekuhn@users.noreply.github.com>
2025-09-01 19:39:48 -0400

    Refactor layout and spacing in App component for improved responsiveness and consistency

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,25 +8,31 @@ function App() {
     <>
       <Navbar />
       <div className="content">
-        <div className={styles.header}>
-          <h1>
-            <MdWavingHand />
-            Welcome!
-          </h1>
-          <h2>
-            Hi, I'm <span className="bold">Linette!</span> I'm a{" "}
-            <span className="bold">CompSci student into web development.</span>
-            <br />I like building cool projects and sharing what I learn along
-            the way!
-          </h2>
-        </div>
-        <div className={styles.links}>
-          <Link className={styles.link} to={"/blog"}>
-            My Blog
-          </Link>
-          <Link className={styles.link} to={"/projects"}>
-            My Projects
-          </Link>
+        <div className={styles.app}>
+          <div className={styles.header}>
+            <h1>
+              <MdWavingHand />
+              Welcome!
+            </h1>
+            <h2>
+              Hi, I'm <span className="bold">Linette!</span> I'm a{" "}
+              <span className="bold">
+                CompSci student into web development.
+              </span>
+            </h2>
+            <h3>
+              I like building cool projects and sharing what I learn along the
+              way!
+            </h3>
+          </div>
+          <div className={styles.links}>
+            <Link className={styles.link} to={"/blog"}>
+              My Blog
+            </Link>
+            <Link className={styles.link} to={"/projects"}>
+              My Projects
+            </Link>
+          </div>
         </div>
       </div>
     </>

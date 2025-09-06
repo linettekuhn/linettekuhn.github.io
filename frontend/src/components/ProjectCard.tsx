baf2bfe458ccac7f3533bc3cd7ffac97525c9ba0
Linette Kühn <139412656+linettekuhn@users.noreply.github.com>
2025-09-05 23:34:47 -0400

    Add card flip animation in Projects page

---
+++ frontend/src/components/ProjectCard.tsx
@@ -0,0 +1,37 @@
+import styles from "./ProjectCard.module.css";
+
+type Props = {
+  name: string;
+  description: string;
+  features: string[];
+  projectLink: string;
+};
+
+export default function ProjectCard({
+  name,
+  description,
+  features,
+  projectLink,
+}: Props) {
+  return (
+    <div
+      className={styles.projectPreview}
+      onClick={() => window.open(projectLink, "_blank", "noopener, noreferrer")}
+    >
+      <div className={styles.projectInner}>
+        <div className={styles.projectFront}>
+          <h3>{name}</h3>
+          <p>{description}</p>
+        </div>
+        <div className={styles.projectBack}>
+          <ul>
+            <h4>Features:</h4>
+            {features.map((feature) => (
+              <li>{feature}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}

25920d392ab931edd3f62193d0a304d043151f3a
Linette Kühn <139412656+linettekuhn@users.noreply.github.com>
2025-09-15 16:16:07 -0400

    Add Smiley component with winking animation for the ü in Kühn

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,11 +2,15 @@ import { Link } from "react-router";
 import styles from "./Navbar.module.css";
 import { BsCursorText } from "react-icons/bs";
 import { FaCode } from "react-icons/fa6";
+import Smiley from "./Smiley";
 
 export default function Navbar() {
   return (
     <nav>
-      <Link to={"/"}>Linette Kühn</Link>
+      <Link to={"/"} className={styles.logo}>
+        Linette K<Smiley />
+        hn
+      </Link>
       <div className={styles.links}>
         <Link to={"/blog"}>
           <BsCursorText />

36fa025ff524cb972ff31f19255ad726309e8978
Linette Kühn <139412656+linettekuhn@users.noreply.github.com>
2025-11-28 22:25:34 -0500

    Redesign project preview cards

--- frontend/src/components/ProjectCard.tsx
+++ frontend/src/components/ProjectCard.tsx
@@ -1,9 +1,14 @@
 import styles from "./ProjectCard.module.css";
+import { FaGithub } from "react-icons/fa";
+import { FaLink } from "react-icons/fa";
 
 type Props = {
   name: string;
   description: string;
   features: string[];
+  highlights: string[];
+  image: string;
+  githubLink: string;
   projectLink: string;
 };
 
@@ -11,25 +16,47 @@ export default function ProjectCard({
   name,
   description,
   features,
+  highlights,
+  image,
+  githubLink,
   projectLink,
 }: Props) {
   return (
-    <div
-      className={styles.projectPreview}
-      onClick={() => window.open(projectLink, "_blank", "noopener, noreferrer")}
-    >
+    <div className={styles.projectPreview}>
       <div className={styles.projectInner}>
         <div className={styles.projectFront}>
-          <h3>{name}</h3>
-          <p>{description}</p>
+          <img src={image} />
+          <div className={styles.projectDetails}>
+            <h3>{name}</h3>
+            <p>{description}</p>
+          </div>
         </div>
         <div className={styles.projectBack}>
-          <ul>
-            <h4>Features:</h4>
-            {features.map((feature, i) => (
-              <li key={i}>{feature}</li>
+          <div className={styles.projectDetails}>
+            <div className={styles.projectHeader}>
+              <h3>{name}</h3>
+              <div className={styles.projectLinks}>
+                <a href={githubLink} target="_blank" rel="noopener noreferrer">
+                  <FaGithub />
+                </a>
+                <a href={projectLink} target="_blank" rel="noopener noreferrer">
+                  <FaLink />
+                </a>
+              </div>
+            </div>
+            <ul className={styles.projectFeatures}>
+              {features.map((feature, i) => (
+                <li key={i}>{feature}</li>
+              ))}
+            </ul>
+          </div>
+          <div className={styles.projectHighlights}>
+            {highlights.map((item, i) => (
+              <p className={styles.projectHighlight} key={i}>
+                {item}
+              </p>
             ))}
-          </ul>
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/pages/Projects.tsx
+++ frontend/src/pages/Projects.tsx
@@ -13,6 +13,8 @@ export default function Projects() {
             key={"EasyCook"}
             name="EasyCook"
             projectLink="https://linettekuhn.github.io/easy-cook"
+            githubLink="https://github.com/linettekuhn/easy-cook"
+            image="/images/easy-cook-project-preview.png"
             description="My first website! A recipe and meal planning web app built with
               React, TypeScript, and Node.js."
             features={[
@@ -22,11 +24,41 @@ export default function Projects() {
               "Automatic grocery list generation",
               "User authentication",
             ]}
+            highlights={[
+              "TypeScript",
+              "React",
+              "Express Backend",
+              "Spoonacular API",
+              "Responsive UI/UX",
+            ]}
+          />
+          <ProjectCard
+            key={"Piggy"}
+            name="Piggy"
+            projectLink="https://linettekuhn.github.io/piggy"
+            githubLink="https://github.com/linettekuhn/piggy"
+            image="/images/piggy-project-preview.png"
+            description="A mobile budgeting app for tracking expenses and visualizing financial built with React Native and Expo."
+            features={[
+              "Visual progress bars and pie charts for spending insights",
+              "Category-based expense tracking",
+              "Cross-platform UI",
+              "Local storage using SQLite for offline functionality",
+            ]}
+            highlights={[
+              "React Native",
+              "TypeScript",
+              "Expo",
+              "SQLite",
+              "Interactive Charts",
+            ]}
           />
           <ProjectCard
             key={"ZeroLio"}
             name="ZeroLío"
             projectLink="https://linettekuhn.github.io/zero-lio"
+            githubLink="https://github.com/linettekuhn/zero-lio"
+            image="/images/zero-lio-project-preview.png"
             description=" A location-based sports field reservation platform built with
               React, TypeScript, and Node.js."
             features={[
@@ -35,11 +67,19 @@ export default function Projects() {
               "Field reservation and scheduling system",
               "Community features for user engagement",
             ]}
+            highlights={[
+              "TypeScript",
+              "React",
+              "Nominatim API",
+              "Geolocation Services",
+            ]}
           />
           <ProjectCard
             key={"TCPChatApp"}
             name="TCP Chat App"
             projectLink="https://linettekuhn.github.io/tcp-chat"
+            githubLink="https://github.com/linettekuhn/tcp-chat"
+            image="/images/tcp-chat-project-preview.png"
             description="A real-time chat application with a C++ TCP server backend and
               React/TypeScript frontend."
             features={[
@@ -48,6 +88,13 @@ export default function Projects() {
               "Active user tracking",
               "Express.js bridge between frontend and C++ server",
               "User authentication",
+            ]}
+            highlights={[
+              "C++ TCP Server",
+              "React",
+              "Sockets",
+              "Multithreading",
+              "Express.js Bridge",
             ]}
           />
         </div>

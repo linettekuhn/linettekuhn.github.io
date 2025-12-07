0aa88f151d2df1c765b1536ea7da6b1e07a7c667
Linette Kühn <139412656+linettekuhn@users.noreply.github.com>
2025-12-06 21:58:52 -0500

    Add about me section and download resume button to contact page

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -38,51 +38,72 @@ export default function Contact() {
       <Navbar />
       <main className={styles.contactPage}>
         <h2>Contact Me</h2>
-        <form className={styles.contactForm} onSubmit={onFormSubmit}>
-          <label htmlFor="name">
-            Name:
-            <input
-              className={styles.input}
-              type="text"
-              name="name"
-              value={name}
-              onChange={(e) => {
-                setName(e.target.value);
-              }}
-            />
-          </label>
-          <label htmlFor="email">
-            Email:
-            <input className={styles.input} type="email" name="email" />
-          </label>
-          <label htmlFor="message" className={styles.message}>
-            Message:
-            <textarea
-              className={styles.input}
-              name="message"
-              id="message"
-            ></textarea>
-          </label>
-          <button className="button">Send Message</button>
-        </form>
-        <div className={styles.socialLinks}>
-          <h3>Social</h3>
-          <a
-            className={styles.github}
-            href="https://github.com/linettekuhn"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaGithubSquare /> GitHub
-          </a>
-          <a
-            className={styles.linkedin}
-            href="https://www.linkedin.com/in/linette-k%C3%BChn-63b176324"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaLinkedin /> LinkedIn
-          </a>
+        <div className={styles.content}>
+          <form className={styles.contactForm} onSubmit={onFormSubmit}>
+            <label htmlFor="name">
+              Name:
+              <input
+                className={styles.input}
+                type="text"
+                name="name"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            </label>
+            <label htmlFor="email">
+              Email:
+              <input className={styles.input} type="email" name="email" />
+            </label>
+            <label htmlFor="message" className={styles.message}>
+              Message:
+              <textarea
+                className={styles.input}
+                name="message"
+                id="message"
+              ></textarea>
+            </label>
+            <button className="button">Send Message</button>
+          </form>
+          <div className={styles.info}>
+            <div className={styles.about}>
+              <h3>About Me</h3>
+              <p>
+                I’m a software developer with hands-on experience in web and
+                mobile development, focused on creating clean, user-centered
+                designs. I love turning complex ideas into simple, intuitive
+                interfaces and constantly improving my work. I’m always
+                learning, always building, and always excited for the next
+                challenge.
+              </p>
+            </div>
+            <div className={styles.resume}>
+              <h3>My Resumé</h3>
+              <a href="/files/resume.pdf" download>
+                <button className="button">Download PDF</button>
+              </a>
+            </div>
+            <div className={styles.socialLinks}>
+              <h3>Social</h3>
+              <a
+                className={styles.github}
+                href="https://github.com/linettekuhn"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithubSquare /> GitHub
+              </a>
+              <a
+                className={styles.linkedin}
+                href="https://www.linkedin.com/in/linette-k%C3%BChn-63b176324"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedin /> LinkedIn
+              </a>
+            </div>
+          </div>
         </div>
         <ToastContainer />
       </main>

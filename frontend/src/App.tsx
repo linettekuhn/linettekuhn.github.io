91de43d36dea1681596e4b393637ac0adb44692f
Linette Kuhn <linette.kuhn@gmail.com>
2026-03-11 15:14:03 -0400

    Add animated gradient backgrounds and interactive mouse effects  (goo/lava lamp effect)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,54 @@
 import { Link } from "react-router";
 import Navbar from "./components/Navbar";
 import styles from "./App.module.css";
-
+import { useEffect, useRef } from "react";
 function App() {
+  const bubbleRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      if (!bubbleRef.current) return;
+
+      bubbleRef.current.style.setProperty("--mouse-x", `${e.clientX}px`);
+      bubbleRef.current.style.setProperty("--mouse-y", `${e.clientY}px`);
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
   return (
     <>
       <Navbar />
       <main className={styles.app}>
+        <svg xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <filter id="goo">
+              <feGaussianBlur
+                in="SourceGraphic"
+                stdDeviation="100"
+                result="blur"
+              />
+              <feColorMatrix
+                in="blur"
+                mode="matrix"
+                values="1 0 0 0 0
+                                                      0 1 0 0 0
+                                                      0 0 1 0 0
+                                                      0 0 0 18 -8"
+                result="goo"
+              />
+              <feBlend in="SourceGraphic" in2="goo" />
+            </filter>
+          </defs>
+        </svg>
+        <div className={styles.gradientsContainer}>
+          <div className={styles.g1}></div>
+          <div className={styles.g2}></div>
+          <div className={styles.g3}></div>
+          <div className={styles.g4}></div>
+          <div ref={bubbleRef} className={styles.interactive}></div>
+        </div>
         <div className={styles.header}>
           <div className={styles.nameWrapper}>
             <h1>
